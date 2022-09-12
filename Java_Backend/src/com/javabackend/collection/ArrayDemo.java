9beab8aaf56f62d3e92a52aef0265e8802e89fcb
viveksingh <viveksingh.pat03@gmail.com>
2022-09-13 03:03:27 +0530

    Refactored

--- Java_Backend/src/com/javabackend/collection/ArrayDemo.java
+++ Java_Backend/src/com/javabackend/collection/ArrayDemo.java
@@ -1,4 +1,4 @@
-package com.collection.arrays;
+package com.javabackend.collection;
 
 public class ArrayDemo {
 
--- Java_Backend/src/com/javabackend/collection/LitIteratorDEmo.java
+++ Java_Backend/src/com/javabackend/collection/LitIteratorDEmo.java
@@ -1,4 +1,4 @@
-package com.collection.arrays;
+package com.javabackend.collection;
 
 import java.util.ArrayList;
 import java.util.HashSet;
